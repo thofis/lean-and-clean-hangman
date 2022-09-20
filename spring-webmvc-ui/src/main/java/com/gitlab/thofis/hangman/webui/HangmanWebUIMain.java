ae45c4270ff2ef81b6b9524680ee081cfd7bf53b
Thomas Fischer <tf@media-nox.de>
2022-09-20 15:43:15 +0200

    initial commit - project structure

---
+++ spring-webmvc-ui/src/main/java/com/gitlab/thofis/hangman/webui/HangmanWebUIMain.java
@@ -0,0 +1,11 @@
+package com.gitlab.thofis.hangman.webui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangmanWebUIMain {
+	public static void main(String[] args) {
+		SpringApplication.run(HangmanWebUIMain.class);
+	}
+}
